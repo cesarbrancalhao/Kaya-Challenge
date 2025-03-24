06d3ea909d8e857fabec984dcb57b81107497ea0
cesarbrancalhao <58019405+cesarbrancalhao@users.noreply.github.com>
2025-03-24 18:30:53 -0300

    Refactor: renamed serializer

--- server/doctor/views.py
+++ server/doctor/views.py
@@ -13,7 +13,7 @@ class DoctorPagination(PageNumberPagination):
 
 class DoctorViewSet(v.ModelViewSet):
     queryset = Doctor.objects.all()
-    srl = DoctorSerializer
+    serializer_class = DoctorSerializer
     svc = DoctorService
     pagination_class = DoctorPagination
 
@@ -29,10 +29,10 @@ class DoctorViewSet(v.ModelViewSet):
             
             page = self.paginate_queryset(doctors)
             if page is not None:
-                serializer = self.srl(page, many=True)
+                serializer = self.serializer_class(page, many=True)
                 return self.get_paginated_response(serializer.data)
             
-            serializer = self.srl(doctors, many=True)
+            serializer = self.serializer_class(doctors, many=True)
             return res(serializer.data, status=stts.HTTP_200_OK)
         except Exception as e:
             return res({'erro': str(e)}, status=stts.HTTP_400_BAD_REQUEST)
@@ -40,7 +40,7 @@ class DoctorViewSet(v.ModelViewSet):
     def retrieve(self, request, pk=None):
         try:
             doctor = self.svc.get_by_id(pk)
-            serializer = self.srl(doctor)
+            serializer = self.serializer_class(doctor)
             return res(serializer.data, status=stts.HTTP_200_OK)
         except Exception as e:
             return res({'erro': str(e)}, status=stts.HTTP_400_BAD_REQUEST)
